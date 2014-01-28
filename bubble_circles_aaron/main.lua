9e3dddc11b2c18fcc72b07077b7363e1c881b9f2
raspiz <a.w.whitmer@eagle.clarion.edu>
2014-01-27 22:59:48 -0500

    added touch event

--- bubble_circles_aaron/main.lua
+++ bubble_circles_aaron/main.lua
@@ -40,37 +40,95 @@ for i=1,100 do
 end
 
 
-local paint = {
-    type = "image",
-    filename = "images/bksq.png",
-    baseDir = system.ResourceDirectory
-}
-
-local rect = display.newRect( 0, 0, 150, 150 )
-
-rect.fill = paint
 
 
-
-local function dropbub()
-
-	local setX = math.random(0, 300) -- lower, upper parameters
-	--local setY = math.random(-25, 25) -- lower, upper parameters
+local function PopBubble(event) -- user touches screen to try to pop a bubble
 	
-	local myCircle = display.newCircle( setX, 10, 15 ) --xloc, yloc, radius(size)
-
-	
-
-
-	
-	--myCircle:setFillColor( 0.5 )
-	myCircle.fill = { type="image", filename="bbub.png" }
-	
-	physics.addBody( myCircle, { density=1, friction=0, bounce=.5 } ) -- with .5 they seem to bouce and settle in better
-
-	myCircle.gravityScale = .25 -- they settle properly at .25 gravity
+	if (event.phase == "began") then
+		event.target:setFillColor(255,255,255) -- turns the touched bubble white
+		-- check to see if contacting other bubbles of the same color
+			-- if touching bubble of same color, have that bubble also do a check
+				-- and etc
+				
+		-- if 3 or more bubbles of the same color are touching, destroy the bubbles
+	end
 
 
 end
 
-local tr = timer.performWithDelay (500, dropbub, 100)	-- delay, function to call, iterations
+
+-- local function onShake (event)
+	-- if event.isShake then
+	--Device was shaken, so do something.
+	-- bubble.gravityScale = -0.5
+	-- end
+-- end
+
+
+
+
+local function DropBubble() -- initially fill the screen with bubbles based on the timer tr
+    --print (number)
+	
+	local setX = math.random(0, 300) -- lower, upper parameters
+	--local setY = math.random(-25, 25) -- lower, upper parameters
+	
+	local bubble = display.newCircle( setX, 10, 15 ) --xloc, yloc, radius(size)
+
+	local bubColor = math.random(0, 3)-- will assign one of four colors to each bubble
+
+
+	
+	
+	if (bubColor == 0) then
+		bubble:setFillColor(0, 0, 255)
+		bubble.color = "blue"
+	elseif (bubColor == 1) then
+		bubble:setFillColor(0, 255, 0)
+		bubble.color = "green"
+	elseif (bubColor == 2) then
+		bubble:setFillColor(255, 0, 0)
+		bubble.color = "red"
+	else-- (bubColor == 3) then
+		bubble:setFillColor(237, 181, 28)	
+		bubble.color = "orange"
+	end
+	
+	
+	--bubble.fill = { type="image", filename="bbub.png" }
+	
+	physics.addBody( bubble, { density=1, friction=0, bounce=.5 } ) -- with .5 they seem to bouce and settle in better
+
+	bubble.gravityScale = .25 -- they settle properly at .25 gravity
+
+	bubble:addEventListener("touch", PopBubble)
+	
+	--bubble:addEventListener("collision", bubble)
+	
+	--bubble:addEventListener("accelerometer", onShake)
+	
+end
+
+
+--function bubble:Collision(event)
+
+
+
+--end
+
+
+
+
+
+
+
+
+
+local tr = timer.performWithDelay (500, DropBubble, 100)	-- delay, function to call, iterations
+
+
+
+
+
+
+
